127772a5c04b8e66cf3744ffddbb4ac954bb062a
LazyMan13 <rwesleyhenderson@gmail.com>
2022-01-26 09:42:22 -0600

    Update my chicken feed code.sql

--- my chicken feed code.sql
+++ my chicken feed code.sql
@@ -1,8 +1,16 @@
 DROP TABLE chicken_feed;
 
+DROP SEQUENCE sq_feed_id;
+
+CREATE SEQUENCE sq_feed_id
+    INCREMENT BY 1
+    MAXVALUE 9999
+    MINVALUE 1000
+    NO CYCLE;
+
 CREATE TABLE chicken_feed
   (
-  feed_id     NUMERIC(4),
+  feed_id     NUMERIC(4) DEFAULT NEXTVAL('sq_feed_id'),
   brand       VARCHAR(30),
   type        VARCHAR(20),
   weight      INT,
