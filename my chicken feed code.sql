d6faa4e05eb3bb099d20632909be852c3c1fd022
LazyMan13 <rwesleyhenderson@gmail.com>
2022-01-31 09:47:46 -0600

    Update my chicken feed code.sql

--- my chicken feed code.sql
+++ my chicken feed code.sql
@@ -15,10 +15,20 @@ CREATE SEQUENCE sq_brand_id
     MINVALUE 20000
     NO CYCLE;
 
+CREATE TABLE manufacturer
+  (
+  brand_id  NUMERIC(5) DEFAULT NEXTVAL('sq_brand_id') PRIMARY KEY,
+  name      VARCHAR(30) UNIQUE,
+  address   VARCHAR(50),
+  city      VARCHAR(30),
+  state     CHAR(2),
+  zip       NUMERIC(5)
+  );
+
 CREATE TABLE chicken_feed
   (
-  feed_id     NUMERIC(5) DEFAULT NEXTVAL('sq_feed_id'),
-  brand_id    NUMERIC(5),
+  feed_id     NUMERIC(5) DEFAULT NEXTVAL('sq_feed_id') PRIMARY KEY,
+  brand_id    NUMERIC(5) REFERENCES manufacturer(brand_id),
   type        VARCHAR(20),
   weight      INT,
   description TEXT,
@@ -27,15 +37,7 @@ CREATE TABLE chicken_feed
   life_stage  VARCHAR(20)
   );
 
-CREATE TABLE manufacturer
-  (
-  brand_id NUMERIC(5) DEFAULT NEXTVAL('sq_brand_id'),
-  name VARCHAR(30),
-  address VARCHAR(50),
-  city VARCHAR(30),
-  state CHAR(2),
-  zip NUMERIC(5)
-  );
+
 
   SELECT *
     FROM chicken_feed;
