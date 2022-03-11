2f2d4ac550b3a5efd33d8f330c90563dd8b0a4b1
AcutusLamia <rwesleyhenderson@gmail.com>
2022-03-11 07:57:18 -0600

    Create chinook_Ch10_Q6_7.sql

---
+++ chinook_Ch10_Q6_7.sql
@@ -0,0 +1,21 @@
+/*Question 6*/
+SELECT ar.name AS "artist_name",COUNT(t.name) AS "num_tracks"
+    FROM track t
+  INNER JOIN album al ON t.albumid = al.albumid
+  INNER JOIN artist ar ON al.artistid = ar.artistid
+  GROUP BY "artist_name"
+    HAVING COUNT(t.name) > 50 AND NOT LOWER(ar.name) IN ('various artists','the office','lost')
+    ORDER BY "num_tracks", "artist_name";
+ 
+/*Question 7*/ 
+SELECT al.title AS "album_title",ar.name AS "artist_name",t.name AS "track_name",mt.name AS "media_type",'$'|| t.unitprice AS "unit_price"
+  FROM Track t 
+  INNER JOIN Album al ON t.albumid = al.albumid 
+  INNER JOIN Artist ar ON al.artistid = ar.artistid 
+  INNER JOIN MediaType mt ON t.mediatypeid = mt.mediatypeid 
+WHERE
+  CASE
+    WHEN LOWER(mt.name) LIKE '%audio%' AND t.unitprice != 0.99 THEN TRUE
+    WHEN LOWER(mt.name) LIKE '%video%' AND t.unitprice != 1.99 THEN TRUE
+  END
+ORDER BY t.trackid;
